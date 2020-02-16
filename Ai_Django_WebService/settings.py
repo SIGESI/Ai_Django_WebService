4161f06a06d48a3aeb65e707245712128f3a342e
SIGESI <songsixte@gmail.com>
2020-02-16 16:14:33 +0100

    add app1 and app2 to INSTALLED_APPS of settings.py

--- Ai_Django_WebService/settings.py
+++ Ai_Django_WebService/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'App1_login_home',
+    'App2_algo1',
 ]
 
 MIDDLEWARE = [
