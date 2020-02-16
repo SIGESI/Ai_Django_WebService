58790ddf453e6af0797b7f2c4c94b5a840055a1e
SIGESI <songsixte@gmail.com>
2020-02-16 15:58:53 +0100

    add algo1 views fonction

--- App2_algo1/views.py
+++ App2_algo1/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+import os
 # Create your views here.
+def algo1(request):
+    #return HttpResponse("myapp2")
+
+    category="cat"
+
+    return render(request, "algo1.html",{"data":category})
+
+def algo1_upload(request):
+
+    File = request.FILES.get("myfile", None)
+    image_path = "./App2_algo1/images/"+"%s"%File.name #+ "%s" % File.name
+
+    # open file to write
+    with open(image_path, 'wb+') as f:
+        # Write to file in chunks
+        for chunk in File.chunks():
+            f.write(chunk)
+    category="dog"
+    return render(request, "algo1.html",{"data":category},locals())
