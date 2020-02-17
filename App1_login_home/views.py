69cd3a411d750ee12774d8282f587d7f9d18f494
SIGESI <songsixte@gmail.com>
2020-02-17 22:35:53 +0100

    add public ip to views's index

--- App1_login_home/views.py
+++ App1_login_home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
-
+from json import load
+from urllib import request as req
 # Create your views here.
 
 
@@ -28,7 +29,12 @@ def login_action(request):
 def index(request):
     #return HttpResponse("myapp2")
 
-    return render(request, "index.html")
+    #ip1=req.urlopen('http://ip.42.pl/raw').read()
+    ip1 = load(req.urlopen('http://jsonip.com'))['ip']
+    #ip1="localhost"
+    #print(ip1)
+    ip1="http://"+str(ip1)+":8001/algo1/"
+    return render(request, "index.html",{'ipalgo1': ip1})
 
 def index_action(request):
     #return HttpResponse("myapp2")
