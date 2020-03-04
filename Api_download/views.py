c384806678379845c9751d8589f3be01ea44259c
SIGESI <songsixte@gmail.com>
2020-03-04 21:18:17 +0100

    add class get_parameter_dic to get request param

--- Api_download/views.py
+++ Api_download/views.py
@@ -5,10 +5,28 @@ from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from django.http import StreamingHttpResponse
 
+from rest_framework.request import Request
+from django.http import QueryDict
+def get_parameter_dic(request, *args, **kwargs):
+    if isinstance(request, Request) == False:
+        return {}
+
+    query_params = request.query_params
+    if isinstance(query_params, QueryDict):
+        query_params = query_params.dict()
+    result_data = request.data
+    if isinstance(result_data, QueryDict):
+        result_data = result_data.dict()
+
+    if query_params != {}:
+        return query_params
+    else:
+        return result_data
+
 class DonwloadView(APIView):
     permission_classes = (AllowAny,)
 
-    def get(self, request, format=None):
+    def get(self, request, format=None, *args, **kwargs,):
 
         def file_iterator(fn, chunk_size=512):
             while True:
@@ -22,7 +40,8 @@ class DonwloadView(APIView):
         #response = StreamingHttpResponse(file_iterator(fn))
         #file = open('f1.file', 'rb')
 
-        f1=File.objects.get(id=3)
+        params = get_parameter_dic(request)
+        f1=File.objects.get(id=params.get("a"))
         response = StreamingHttpResponse(f1.file)
         name=f1.file.name
         response['Content-Type'] = 'application/octet-stream'
